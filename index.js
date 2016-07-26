df9cb0606768eacfb5d7a3d725e05714491bd787
David Thibault <thibault.david@gmail.com>
2016-07-26 17:21:36 -0400

    Define basic app routes

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function (req, res) {
+
+});
+
+app.get("/:boardId", function (req, res) {
+
+});
+
+app.get("/:boardId/player", function (req, res) {
+
+});
