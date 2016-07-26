769cea08dda50d512a7d708ed18e38080fea160c
David Thibault <thibault.david@gmail.com>
2016-07-26 18:00:36 -0400

    Install handlebars and create home view

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-var express = require("express");
+var express = require("express"), 
+	exphbs  = require("express-handlebars");
+
 var app = express();
 
-app.get("/", function (req, res) {
+app.use(express.static('public'));
 
+app.engine("handlebars", exphbs({defaultLayout:"layout"}));
+app.set("view engine", "handlebars")
+
+app.get("/", function (req, res) {
+	res.render("home");
 });
 
 app.get("/:boardId", function (req, res) {
-
+	res.render("board");
 });
 
 app.get("/:boardId/player", function (req, res) {
+	res.render("player");
+});
 
+app.listen(3000, function() {
+	console.log("Listening on port 3000")
 });
